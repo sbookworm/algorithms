9fb262f4409edc46646c7b7e3634d6aa8a209a6a
song <lisong.cruise@gmail.com>
2016-09-26 14:56:27 +0800

    add pop action and test

--- minstack/minstack.py
+++ minstack/minstack.py
@@ -33,39 +33,96 @@ def init_min_stack(arr):
     return arr
 
 
+def insert(v):
+    pass
+
+
+def pop_min(arr):
+    min = arr[0]
+    last = arr.pop()
+    arr[0] = last
+    arr = init_min_stack(arr)
+    return min
+
+
+def test_pop_min():
+    test_result = 0
+    for i in range(50, 500):
+        test_arr = []
+        for j in range(i):
+            test_arr.append(random.randint(0,1000))
+
+        mstack = init_min_stack(test_arr)
+        for k in range(5):
+            before_len = len(mstack)
+            min = pop_min(mstack)
+            after_len = len(mstack)
+            for i in range(len(test_arr)):
+                if mstack[i] < min:
+                    test_result = 1
+                    print "failed in compare"
+                    break
+                if before_len != (after_len + 1):
+                    test_result = 1
+                    print "failed in length"
+                    break
+
+                if test_result == 0:
+                    test_result = test_min_stack_fine(mstack)
+                    if test_result == 1:
+                        print "failed in update stack"
+                        break
+
+            if test_result == 1:
+                break
+
+    if test_result == 1:
+        print "failed pop action test!"
+    else:
+        print "passed pop action test!"
+
+
+def test_min_stack_fine(arr):
+    test_result = 0
+    arr_len = len(arr)
+    for k in range(arr_len/2):
+        child_1 = 2*k + 1
+        child_2 = 2*k + 2
+        if child_1 >= arr_len:
+            break
+        if arr[child_1] < arr[k]:
+            test_result = 1
+            break
+
+        if child_2 >= arr_len:
+            break
+        if arr[child_2] < arr[k]:
+            test_result = 1
+            break
+
+    return test_result
+
 def test_init_min_stack():
     test_result = 0
-    for i in range(50,100):
+    for i in range(50, 500):
         stack_arr = []
-        for j in range(3,100):
+        for j in range(i):
             #stack_arr.append(j)
             stack_arr.append(random.randint(0,500))
 
         arr = init_min_stack(stack_arr)
         #min = arr[0]
-        arr_len = len(arr)
-        for k in range(arr_len/2):
-            child_1 = 2*k + 1
-            child_2 = 2*k + 2
-            if child_1 >= arr_len:
-                break
-            if arr[child_1] < arr[k]:
-                test_result = 1
-                break
-
-            if child_2 >= arr_len:
-                break
-
-            if arr[child_2] < arr[k]:
-                test_result = 1
-                break
+        test_result = test_min_stack_fine(arr)
+        if test_result == 1:
+            break
 
     if test_result == 1:
-        print "failed test!"
+        print "failed init min stack test!"
     else:
-        print "passed test!"    
+        print "passed init min stack test!"
 
 
 if __name__ == '__main__':
     test_init_min_stack()
 
+    test_pop_min()
