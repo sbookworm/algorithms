f92d798c9b45e660b23f76a3dea74b569fe419db
lisong <lisong@zhongruigroup.com>
2018-12-11 18:06:43 +0800

    add a little

--- tianchi/recommend/level_1/data_formate.py
+++ tianchi/recommend/level_1/data_formate.py
@@ -1,15 +1,16 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 
+# simple similar, not using vector as too many items might cause memory crash
 def calc_similar(u, v):
     count = 0
     for i in u:
         if i in v:
             count += 1
-    return count
+    return count/(len(u) + len(v))
 
 
-# recommon u by v
+# recommend u by v
 def recommend(u, v):
     rec_lst = []
     u_lst = user_dict[u]
@@ -20,7 +21,6 @@ def recommend(u, v):
 
     return rec_lst
 
-#item_category = {}
 """
 with open("../tianchi_fresh_comp_train_item.csv", 'r') as item:
     title = item.readline()
@@ -33,44 +33,46 @@ with open("../tianchi_fresh_comp_train_item.csv", 'r') as item:
         item_category[d[0]] = d[2]
 """
 
-user_dict = {}
-with open("../tianchi_fresh_comp_train_user.csv", 'r') as user:
-    title = user.readline()
-    while 1:
-        l = user.readline()
-        if not l:
-            break
-        l = l.strip()
-        d = l.split(",")
-        uid = d[0]
-        iid = d[1]
-        bhvr = d[2]
-        if bhvr > 1:
-            user_dict.setdefault(uid, [])
-            user_dict[uid].append(iid)
+if __name__ == '__main__':
 
-
-user_similar = {}
-for u in user_dict.keys():
-    user_similar.setdefault(u, {})
-    for v in user_dict.keys():
-        if u == v:
-            continue
-        user_similar[u].setdefault(v, 0)
-        user_similar[u][v] = calc_similar(user_dict[u], user_dict[v])
-
-for u in user_similar.keys():
-    max_u = 0
-    sim_u = u
-    for v in user_similar[u].keys():
-        if u == v:
-            continue
-        if user_similar[u][v] > max_u:
-            max_u = user_similar[u][v]
-            sim_u = v
-    if max_u == 0:
-        continue
+    user_dict = {}
+    with open("../tianchi_fresh_comp_train_user.csv", 'r') as user:
+        title = user.readline()
+        while 1:
+            l = user.readline()
+            if not l:
+                break
+            l = l.strip()
+            d = l.split(",")
+            uid = d[0]
+            iid = d[1]
+            bhvr = d[2]
+            if bhvr > 1:
+                user_dict.setdefault(uid, [])
+                user_dict[uid].append(iid)
     
-    lst = recommend(u, max_u)
-    for  i in lst:
-        print "%s,%s" % (u, lst[i])
+    
+    user_similar = {}
+    for u in user_dict.keys():
+        user_similar.setdefault(u, {})
+        for v in user_dict.keys():
+            if u == v:
+                continue
+            user_similar[u].setdefault(v, 0)
+            user_similar[u][v] = calc_similar(user_dict[u], user_dict[v])
+    
+    for u in user_similar.keys():
+        max_u = 0
+        sim_u = u
+        for v in user_similar[u].keys():
+            if u == v:
+                continue
+            if user_similar[u][v] > max_u:
+                max_u = user_similar[u][v]
+                sim_u = v
+        if max_u == 0:
+            continue
+        
+        lst = recommend(u, max_u)
+        for  i in lst:
+            print "%s,%s" % (u, lst[i])
