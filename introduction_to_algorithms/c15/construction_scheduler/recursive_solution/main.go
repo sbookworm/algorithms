997b91b87b9f8764ed5bdd8196d88c9ec40c33d1
sbookworm <lisong.cruise@gmail.com>
2021-11-13 09:54:16 +0800

    init introduction to algorithms c15

---
+++ introduction_to_algorithms/c15/construction_scheduler/recursive_solution/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	s1 := []int{7, 9, 3, 4, 8, 4}
+	s2 := []int{8, 5, 6, 4, 5, 7}
+
+	t1 := []int{2, 3, 1, 3, 4}
+	t2 := []int{2, 1, 2, 2, 1}
+
+	fmt.Println("recursive solution of construction scheduler")
+}
